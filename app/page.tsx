a6c932fc1232ab7e74bbb0ecc793215f54ff8b2c
ZidanAK22 <zidanazrak@gmail.com>
2024-01-09 13:35:25 +0700

    Final

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,3 @@
-import { Card, CardContent } from "@/components/ui/card"
-
-import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
-} from "@/components/ui/carousel"
-
 import Image from "next/image"
 
 export default function Home() {
